7fa4516f1d3b0070245df114928e57c48b34df75
abanoub soliman <bibosoliman20@icloud.com>
2023-06-24 20:50:32 -0700

    tasl 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 import sys
 
-# Retrieve the command-line arguments
-argv = sys.argv[1:]
+if __name__ == "__main__":
+    # Retrieve the command-line arguments
+    argv = sys.argv[1:]
 
-# Get the number of arguments
-num_args = len(argv)
+    # Get the number of arguments
+    num_args = len(argv)
 
-# Print the number of arguments
-print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}{'.' if num_args == 0 else ':'}")
+    # Print the number of arguments
+    print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}{'.' if num_args == 0 else ':'}")
 
-# Print each argument
-for i, arg in enumerate(argv, 1):
-    print(f"{i}: {arg}")
+    # Print each argument
+    for i, arg in enumerate(argv, 1):
+        print(f"{i}: {arg}")
